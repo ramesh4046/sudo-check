5b00a7ae962ad183c6aca32a61c35e32b4486281
ramesh4046 <ramesh4046@gmail.com>
2016-07-12 16:27:42 -0500

    Added files

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+name             "sudo"
+maintainer       "Meetup"
+maintainer_email "qing@meetup.com"
+license          "Apache 2.0"
+description      "Installs/Configures sudo"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
